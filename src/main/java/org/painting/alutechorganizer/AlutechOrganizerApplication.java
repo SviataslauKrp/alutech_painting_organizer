577ddf5a1162bc3ed6cd5c48fee1a81b167d3c10
Sviataslau <kirpik.svyatoslav@yandex.ru>
2023-03-17 21:48:34 +0300

    init commit

---
+++ src/main/java/org/painting/alutechorganizer/AlutechOrganizerApplication.java
@@ -0,0 +1,13 @@
+package org.painting.alutechorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlutechOrganizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlutechOrganizerApplication.class, args);
+    }
+
+}
